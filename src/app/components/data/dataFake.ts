0e470550fb57b886e83ed8eda7e8b1af4c6d0b2a
Bfd00 <felipefernandesbs@gmail.com>
2023-09-01 14:22:06 -0300

    content created

---
+++ src/app/components/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    Id: '1',
+    title: 'NOVO HOMEM DE FERRO EM 3D',
+    description: 'marvel anuncia o novo filme do homem de ferro confira',
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxW_48nVEZuqHJqfMNJ7qZoiUkb388E4JEIg&usqp=CAU',
+  },
+  {
+    Id: '2',
+    title: 'Nova serie anunciada no Disney +',
+    description: 'ola',
+    photoCover:
+      'https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg',
+  },
+  {
+    Id: '3',
+    title: 'Novo Filme do Pantera Negra será lançado em breve',
+    description: 'ola',
+    photoCover:
+      'https://i0.wp.com/metagalaxia.com.br/wp-content/uploads/2022/11/pantera-negra-1.webp?fit=1920%2C1080&ssl=1',
+  },
+  {
+    Id: '4',
+    title: 'Novo Filme dos X-men está em pós produção afirma diretor',
+    description: 'ola',
+    photoCover:
+      'https://s2-techtudo.glbimg.com/XEjoJ9NZIfzAak4yzk_nxl-urtI=/0x0:2560x1600/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/o/o/RWzATXSaG30IWVkdLHwQ/x-men-capa.jpg',
+  },
+];
